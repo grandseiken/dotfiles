d68d75e0ffef30976478d89f90638fb9318d5500
stu <s+github@stu.scot>
2015-05-24 22:13:32 +0100

    Make vim solarized.

--- make_links.sh
+++ make_links.sh
@@ -1,8 +1,8 @@
 # Make symlinks to files in home directory.
-TARGETS=".i3/config .vimrc .conkyrc conky.sh"
+TARGETS=".i3/config .vim/colors .vimrc .conkyrc conky.sh"
 
 for TARGET in $TARGETS; do
-  DOTFILE=$(dirname $0)/$TARGET
+  DOTFILE=$(pwd)/$(dirname $0)/$TARGET
   LINK=~/$TARGET
   rm -rf $LINK
   mkdir -p $(dirname $LINK)
