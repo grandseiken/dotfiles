34c643ece58c7833cd5160992caa494d8415522d
stu <s+github@stu.scot>
2015-05-24 22:31:35 +0100

    Move to install.sh and update with prerequisites.

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+# Prerequisites:
+sudo apt-get install i3 i3lock zsh
+wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 # Make symlinks to files in home directory.
 TARGETS=".i3/config .vim/colors .vimrc .conkyrc conky.sh"
 
